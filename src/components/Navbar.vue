11a51043716e89b0b5734d772f3b0a65770bf1d9
SacHin Yadav <sachineg39@gmail.com>
2020-08-02 17:08:08 +0530

    lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,16 @@
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" class="success" app>
-            <p class="info">test</p>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon  class="white--text">{{link.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -23,6 +32,11 @@ export default {
     data(){
         return{
             drawer:false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My Projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' },
+            ]
         }
     }
 };
