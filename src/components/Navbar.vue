b6bef11b37fbb2dddb91269ed0dce0388928850f
SacHin Yadav <sachineg39@gmail.com>
2020-08-02 16:39:34 +0530

    Toolbars

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <v-app-bar flat app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Salvan</span>
+                <span>Todo</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
