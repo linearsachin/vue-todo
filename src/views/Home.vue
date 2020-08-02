72600fd3fa635c00cc60d8f83a1fc876704838e9
SacHin Yadav <sachineg39@gmail.com>
2020-08-02 15:47:43 +0530

    text and colours

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>This is Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime est commodi deleniti perferendis facere assumenda culpa laborum ducimus explicabo sunt itaque voluptate perspiciatis animi earum inventore magnam necessitatibus, quibusdam harum.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt repudiandae voluptatum placeat deserunt qui, porro ipsam itaque? Ex, hic sed. Ea doloremque cum adipisci asperiores rem officia, eveniet culpa sed.</p>
+    <h1 class="display-4">Massive Text</h1>
+    <h4 class="display-1">Smaller Text</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheadline">This is a subheadline</p>
+    <p class="caption text-uppercase">This is a caption</p>
   </div>
 </template>
 
