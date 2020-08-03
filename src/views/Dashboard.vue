d9b7efd657f06dc33f7dd0f4df3cee97f7c1b28a
SacHin Yadav <sachineg39@gmail.com>
2020-08-03 19:11:22 +0530

    grid1

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,33 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Homepage</h1>
     <v-container class="my-5">
-      content
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn  block class="primary" outlined>1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block class="primary">3</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outlined block class="primary">4</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around="">
+        <v-flex xs4 md3>
+          <v-btn outlined block color="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outlined block color="success">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outlined block color="success">3</v-btn>
+        </v-flex>
+
+      </v-layout>
     </v-container>
 
 
