572db2d06ed2d77cdb18063b2ea9169165420c4b
SacHin Yadav <sachineg39@gmail.com>
2020-08-05 19:51:58 +0530

    avatar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,17 @@
             </v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer v-model="drawer" class="success" app>
+        <v-navigation-drawer v-model="drawer" class="primary" app>
+            <v-layout column align-center>
+                <v-flex class="mt-8">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                    <p class="white--text subheading mt-1 text-center">
+                        The Salvan
+                    </p>
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
